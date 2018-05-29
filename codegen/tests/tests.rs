147511b4db9d7330f33cce7c3cc1eb0fdba255f4
Sergio Benitez <sb@sergio.bz>
2018-05-29 17:38:42 +0200

    Improve compiletest codegen test dependency resolution.
    In this commit, Rocket's codegen tests now search for the correct
    library to depend on, independent of compiletest. This solution should
    be robust against platform differences.

--- codegen/tests/tests.rs
+++ codegen/tests/tests.rs
@@ -1,18 +1,41 @@
 extern crate compiletest_rs as compiletest;
 
+use std::io;
 use std::path::{Path, PathBuf};
 
-fn link_flag(flag: &str, lib: &str, rel_path: &[&str]) -> String {
-    let environment = match cfg!(debug_assertions) {
-        true => "debug",
-        false => "release"
-    };
+#[derive(Copy, Clone)]
+enum Kind {
+    Dynamic, Static
+}
 
-    let mut path = Path::new(env!("CARGO_MANIFEST_DIR"))
+impl Kind {
+    fn extension(self) -> &'static str {
+        match self {
+            #[cfg(windows)] Kind::Dynamic => ".dll",
+            #[cfg(all(unix, target_os = "macos"))] Kind::Dynamic => ".dylib",
+            #[cfg(all(unix, not(target_os = "macos")))] Kind::Dynamic => ".so",
+            Kind::Static => ".rlib"
+        }
+    }
+
+    fn prefix(self) -> &'static str {
+        #[cfg(windows)] { "" }
+        #[cfg(not(windows))] { "lib" }
+    }
+}
+
+fn target_path() -> PathBuf {
+    #[cfg(debug_assertions)] const ENVIRONMENT: &str = "debug";
+    #[cfg(not(debug_assertions))] const ENVIRONMENT: &str = "release";
+
+    Path::new(env!("CARGO_MANIFEST_DIR"))
         .parent().unwrap()
         .join("target")
-        .join(environment);
+        .join(ENVIRONMENT)
+}
 
+fn link_flag(flag: &str, lib: &str, rel_path: &[&str]) -> String {
+    let mut path = target_path();
     for component in rel_path {
         path = path.join(component);
     }
@@ -20,18 +43,46 @@ fn link_flag(flag: &str, lib: &str, rel_path: &[&str]) -> String {
     format!("{} {}={}", flag, lib, path.display())
 }
 
+fn extern_dep(name: &str, kind: Kind) -> io::Result<String> {
+    let deps_root = target_path().join("deps");
+    let dep_name = format!("{}{}", kind.prefix(), name);
+
+    let mut dep_path: Option<PathBuf> = None;
+    for entry in deps_root.read_dir().expect("read_dir call failed") {
+        let entry = entry?;
+        let filename = entry.file_name();
+        let filename = filename.to_string_lossy();
+        let lib_name = filename.split('.').next().unwrap().split('-').next().unwrap();
+
+        if lib_name == dep_name && filename.ends_with(kind.extension()) {
+            if let Some(ref mut existing) = dep_path {
+                if entry.metadata()?.created()? > existing.metadata()?.created()? {
+                    *existing = entry.path().into();
+                }
+            } else {
+                dep_path = Some(entry.path().into());
+            }
+        }
+    }
+
+    let dep = dep_path.ok_or_else(|| io::Error::from(io::ErrorKind::NotFound))?;
+    let filename = dep.file_name().ok_or_else(|| io::Error::from(io::ErrorKind::InvalidData))?;
+    Ok(link_flag("--extern", name, &["deps", &filename.to_string_lossy()]))
+}
+
 fn run_mode(mode: &'static str) {
     let mut config = compiletest::Config::default();
-    let cfg_mode = mode.parse().expect("Invalid mode");
+    config.mode = mode.parse().expect("Invalid mode");
+    config.src_base = format!("tests/{}", mode).into();
+    config.clean_rmeta();
 
-    config.mode = cfg_mode;
-    config.src_base = PathBuf::from(format!("tests/{}", mode));
+    config.target_rustcflags = Some([
+        link_flag("-L", "crate", &[]),
+        link_flag("-L", "dependency", &["deps"]),
+        extern_dep("rocket_codegen", Kind::Dynamic).expect("find codegen dep"),
+        extern_dep("rocket", Kind::Static).expect("find core dep")
+    ].join(" "));
 
-    let crate_path = link_flag("-L", "crate", &[]);
-    let dep_path = link_flag("-L", "dependency", &["deps"]);
-    let codegen_dep = link_flag("--extern", "rocket_codegen", &["deps", "librocket_codegen.dylib"]);
-
-    config.target_rustcflags = Some([codegen_dep, dep_path, crate_path].join(" "));
     compiletest::run_tests(&config);
 }
 
