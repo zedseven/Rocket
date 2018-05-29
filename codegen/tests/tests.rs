a13a4e258abb789708672c4578cb593bcb9ec77b
Sergio Benitez <sb@sergio.bz>
2018-05-29 14:57:48 +0200

    Fix compiletest codegen test dependency resolution.

--- codegen/tests/tests.rs
+++ codegen/tests/tests.rs
@@ -1,6 +1,24 @@
 extern crate compiletest_rs as compiletest;
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+fn link_flag(flag: &str, lib: &str, rel_path: &[&str]) -> String {
+    let environment = match cfg!(debug_assertions) {
+        true => "debug",
+        false => "release"
+    };
+
+    let mut path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent().unwrap()
+        .join("target")
+        .join(environment);
+
+    for component in rel_path {
+        path = path.join(component);
+    }
+
+    format!("{} {}={}", flag, lib, path.display())
+}
 
 fn run_mode(mode: &'static str) {
     let mut config = compiletest::Config::default();
@@ -8,8 +26,12 @@ fn run_mode(mode: &'static str) {
 
     config.mode = cfg_mode;
     config.src_base = PathBuf::from(format!("tests/{}", mode));
-    config.link_deps();
 
+    let crate_path = link_flag("-L", "crate", &[]);
+    let dep_path = link_flag("-L", "dependency", &["deps"]);
+    let codegen_dep = link_flag("--extern", "rocket_codegen", &["deps", "librocket_codegen.dylib"]);
+
+    config.target_rustcflags = Some([codegen_dep, dep_path, crate_path].join(" "));
     compiletest::run_tests(&config);
 }
 
