368f387c21cf73eed61ab3e8f38a2a62b4e4edfc
Sergio Benitez <sb@sergio.bz>
2020-02-09 01:52:47 -0800

    Fix tests for latest nightly.

--- scripts/test.sh
+++ scripts/test.sh
@@ -41,7 +41,7 @@ function ensure_tab_free() {
 
 # Ensures there are no files with trailing whitespace.
 function ensure_trailing_whitespace_free() {
-  local matches=$(git grep -E -I "\s+$" "${PROJECT_ROOT}")
+  local matches=$(git grep -E -I "\s+$" "${PROJECT_ROOT}" | grep -v '.stderr:')
   if ! [ -z "${matches}" ]; then
     echo "Trailing whitespace was found in the following:"
     echo "${matches}"
